264affdc3c51d2454ebd91ffc6aa9e991859e4d4
Zihao Gao <zhgao340@hotmail.com>
2024-11-25 13:55:37 +0900

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Zihao Gao on 2024/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
